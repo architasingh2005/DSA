3379474e2fb03b3707dfe69d553ab5737c08dbb5
architasingh2005 <architasingh889@gmail.com>
2025-12-06 17:05:23 +0530

    Time: 2 ms (38.55%) | Memory: 45.5 MB (21.15%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -4,7 +4,7 @@ class Solution {
     int n;
     List<List<Integer>> result;
 
-    void solve(List<Integer> temp, boolean[] used, int[] nums) {
+   /* void solve(List<Integer> temp, boolean[] used, int[] nums) {
         if (temp.size() == n) {
             result.add(new ArrayList<>(temp));
             return;
@@ -29,6 +29,34 @@ class Solution {
         result = new ArrayList<>();
         boolean[] used = new boolean[n];
         solve(new ArrayList<>(), used, nums);
+        return result;*/
+
+        // using set
+        void solve(List<Integer> temp, Set<Integer> used, int[] nums) {
+        if (temp.size() == n) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (used.contains(nums[i])) continue; // skip if already used
+
+            temp.add(nums[i]);
+            used.add(nums[i]);
+
+            solve(temp, used, nums);
+
+            // backtrack
+            temp.remove(temp.size() - 1);
+            used.remove(nums[i]);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        n = nums.length;
+        result = new ArrayList<>();
+        solve(new ArrayList<>(), new HashSet<>(), nums);
         return result;
     }
 }
+
